a7c4f15e671778f160f8443fafe4da08a67231a3
SamP2000 <sam.putnam@unb.ca>
2023-03-21 12:10:18 -0300

    1.16 (class is complete, not sure if a driver is needed)

--- src/main/TwoChoicesHashtable.java
+++ src/main/TwoChoicesHashtable.java
@@ -7,13 +7,9 @@ public class TwoChoicesHashtable {
 	private class Node{
 		public String key;
 		public Node next = null;
-		
-		public Node(String key) {
-			this.key = key;
-		}
 	}
 	
-	public int bucketCount(int index) {
+	private int bucketCount(int index) {	//helper method to count collisions
 		int count = 0;
 		Node pointer = table.get(index);
 		while(pointer != null) {
@@ -23,7 +19,15 @@ public class TwoChoicesHashtable {
 		return count;
 	}
 	
-	public int hashcode(String key) {			//sum ascii values of all chars
+	private Node lastNode(int i) {			//helper method to retrieve the last node of a bucket
+		Node temp = table.get(i);
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+	
+	public int hashcode(String key) {		//method: sum the ascii values of all chars
 		int hashcode = 0;
 		for (int i=0; i<key.length(); i++) {
 			hashcode += key.charAt(i);
@@ -39,24 +43,26 @@ public class TwoChoicesHashtable {
 		return ((2*hashcode) % 10);
 	}
 	
-	public void add(String key) {
-		Node toAdd = new Node(key);
-		int index1 = hash1(hashcode(key));
-		int index2 = hash2(hashcode(key));
-		if (table.get(index1) != null && table.get(index2) != null) { //both buckets occupied
-			if (bucketCount(index1) < bucketCount(index2)) {
-				
+	public void add(Node toAdd) {
+		int index1 = hash1(hashcode(toAdd.key));
+		int index2 = hash2(hashcode(toAdd.key));
+		if (table.get(index1) != null && table.get(index2) != null) {	//both buckets occupied
+			if (bucketCount(index1) > bucketCount(index2)) {	//right has less collisions
+				lastNode(index2).next = toAdd;
+			}
+			else {												//default: insert at left
+				lastNode(index1).next = toAdd;
 			}
 		}
-		else if (table.get(index1) == null)	//first bucket empty
+		else if (table.get(index1) == null)								//left bucket empty
 			table.add(index1, toAdd);
-		else if (table.get(index2) == null) //second bucket empty
+		else if (table.get(index2) == null)								//right bucket empty
 			table.add(index2, toAdd);
 	}
 	
 	public static void main(String[] args) {
 		
-		table = new ArrayList<Node>(10);
+		// Not sure if I need to make a driver for this
 		
 	}
 }
